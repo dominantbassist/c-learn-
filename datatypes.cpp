4fd13a88cfa8cdc812242717524e64797f12d6f4
dominantbassist <ali.salehibass.1995@gmail.com>
2024-01-25 09:48:59 +0330

    [ADD] datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     // comment
+     
+     /*
+     multiline
+     comment
+     */
+     
+     int size = 40;
+     double food = 2.5;
+     float sugar = 8.25556251245;
+     char firstLetterOfEnglish = 'A';
+     int age;
+     string name;
+     string city = "Tehran";
+     
+     cout << "what is your name?";
+     cin >> name;
+     cout << "so glad to see you "<< name;
+}
